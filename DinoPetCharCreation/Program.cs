65ebbe79e7ac27b2df733bd7a906f5c4420b9047
Andrei <andreidimasaka@gmail.com>
2024-11-29 11:15:31 +0800

    start

--- DinoPetCharCreation/Program.cs
+++ DinoPetCharCreation/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace DinoPetCharCreation
+{
+    public class Menu
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Breed");
+        }
+    }
+}
